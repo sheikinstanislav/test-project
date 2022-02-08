867d41cba01204e0d557cb00afbdcb14b11a88d6
sheikinstanislav <sheikinstanislav@gmail.com>
2022-02-08 21:05:08 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ const tl = gsap.timeline({
   }
 });
 
-tl.fromTo(".service-item__img", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
-  .fromTo(".service-item__title", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
-  .fromTo(".service-item__desc", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+tl.fromTo(".service-item__img", 0.7, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".service-item__title", 0.7, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".service-item__desc", 0.7, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
   .fromTo(".lightpaper-img", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
   .fromTo(".section-heading2", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
   .fromTo(".section-heading3", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
