315fc7b52a74a31de2323a0e268d52802d690ab2
sheikinstanislav <sheikinstanislav@gmail.com>
2022-02-08 20:59:10 +0200

    test project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Custom Scripts
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(".section-heading1", 1.5, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "service",
+  }
+});
+
+tl.fromTo(".service-item__img", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".service-item__title", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".service-item__desc", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".lightpaper-img", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".section-heading2", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+  .fromTo(".section-heading3", 1, {opacity: 0, y: '70px'}, {opacity: 1, y: '0'})
+
